791f22baec74330e9448e00331eaa7f242bb5cdb
Acquiesced <itsbradchow@gmail.com>
2015-11-30 10:49:59 -0500

    messed up changes for build paths again

---
+++ src/project/Query.java
@@ -0,0 +1,130 @@
+package src.project;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
+public class Query {
+
+	HashMap<String, Double> weights;
+	private double normalizedLength;
+	private int id;
+	private String queryText;
+	private static final String STOPWORDSFILE = "stopwords.txt";
+	private List<String> stopWordsList = new ArrayList<String>();
+
+	@Override
+	public String toString() {
+		return "Query [id=" + id + ", queryText=" + queryText + "]";
+	}
+
+	public Query(String queryText) {
+		this.queryText = queryText;
+		weights = new HashMap<String, Double>();
+	}
+
+	public Query(int id) {
+		this.id = id;
+		weights = new HashMap<String, Double>();
+	}
+
+	public Collection<String> getTerms() {
+		return weights.keySet();
+	}
+
+	public Collection<Double> getWeights() {
+		return weights.values();
+	}
+
+	public double getWeight(String term) {
+		return (weights.get(term) == null) ? 0 : weights.get(term);
+	}
+
+	public double getNormalizedLength() {
+		return normalizedLength;
+	}
+
+	public void setNormalizedLength(double normalizedLength) {
+		this.normalizedLength = normalizedLength;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQueryText() {
+		return queryText;
+	}
+
+	public void setQueryText(String queryText) {
+		this.queryText = queryText;
+	}
+
+	public void initializeStopWordsList() {
+		try {
+			stopWordsList = Files.readAllLines(new File(STOPWORDSFILE).toPath(), StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void calculateQueryWeights(TreeMap<String, DictionaryRecord> dictionary, boolean useStopWords, double idfThreshold) {
+		// the weight of the query term is the idf
+		String[] words = this.queryText.split("[^\\w/'-]+");
+
+		Set<String> uniqueWords = new HashSet<String>(Arrays.asList(words));
+
+		HashMap<String, Integer> termFrequencies = new HashMap<String, Integer>();
+
+		if (useStopWords) {
+			initializeStopWordsList();
+		}
+
+		for (String word : words) {
+			if (useStopWords && stopWordsList.contains(word)) {
+				uniqueWords.remove(word);
+				continue;
+			}
+
+			if (dictionary.containsKey(word)) {
+				DictionaryRecord dictRec = dictionary.get(word);
+				double docFreq = dictRec.getDocumentFrequency();
+				double idf = Math.log10(dictionary.size() / docFreq);
+				if (idf < idfThreshold) {
+					uniqueWords.remove(word);
+					continue;
+				}
+			}
+
+			if (!termFrequencies.containsKey(word)) {
+				termFrequencies.put(word, 1);
+			} else {
+				termFrequencies.put(word, termFrequencies.get(word) + 1);
+			}
+		}
+
+		for (String word : uniqueWords) {
+			int freq = termFrequencies.get(word);
+			double weight = 1 + Math.log10((double) freq);
+			this.weights.put(word, 1 + Math.log10(weight));
+			this.normalizedLength += weight * weight;
+		}
+
+		this.normalizedLength = Math.sqrt(normalizedLength);
+	}
+
+}
---
+++ src/project/Test.java
@@ -0,0 +1,125 @@
+package src.project;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.TreeSet;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+public class Test {
+	private static ArrayList<Double> timeList = new ArrayList<Double>();
+	private static final String file = "cacm/cacm.all";
+	//private static final String file = "cacm/cacm.small2";
+
+	static HashMap<Integer, DocumentRecord> documents = new HashMap<Integer, DocumentRecord>();
+	static TreeMap<String, DictionaryRecord> dictionary = new TreeMap<String, DictionaryRecord>();
+	static TreeMap<String, TreeMap<Integer, Posting>> postingsList = new TreeMap<String, TreeMap<Integer, Posting>>();
+
+	public static double calculateAverage() {
+		if (timeList.size() == 0)
+			return 0;
+		double total = 0;
+		for (double time : timeList) {
+			total += time;
+		}
+		return total / timeList.size();
+	}
+
+	public static void main(String[] args) {
+		boolean useStemming = false;
+		boolean useStopWords = false;
+		long startTime = System.nanoTime();
+		System.out.println("Welcome!");
+
+		Invert inverter = new Invert(file);
+		double idfThreshold = 1.0;
+		int maxResults = 15;
+
+		Options options = new Options();
+		options.addOption("stem", false, "Enable stemming");
+		options.addOption("stopwords", false, "Enable stop word removal");
+		options.addOption("create", false, "Create index input files");
+		options.addOption(OptionBuilder.withLongOpt("idf").withDescription("Idf threshold").withType(Number.class)
+				.hasArg().withArgName("argname").create());
+		options.addOption(
+				OptionBuilder.withLongOpt("maxresults").withDescription("Maximum number of entries to retrieve")
+						.withType(Number.class).hasArg().withArgName("argname").create());
+		options.addOption("help", "Show this menu");
+
+		CommandLineParser parser = new DefaultParser();
+		try {
+			CommandLine cmd = parser.parse(options, args);
+			if (cmd.hasOption("stem")) {
+				inverter.setUseStemming(true);
+				useStemming = true;
+			}
+			if (cmd.hasOption("stopwords")) {
+				inverter.setUseStopWords(true);
+				useStopWords = true;
+			}
+			if (cmd.hasOption("create")) {
+				inverter.setCreateNewIndex(true);
+			}
+			if (cmd.hasOption("idf")) {
+				idfThreshold = ((Number) cmd.getParsedOptionValue("idf")).doubleValue();
+			}
+			if (cmd.hasOption("maxresults")) {
+				maxResults = ((Number) cmd.getParsedOptionValue("maxresults")).intValue();
+			}
+			if (cmd.hasOption("help")) {
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.printHelp("Test for Invert", options);
+				System.exit(0);
+			}
+		} catch (ParseException e) {
+			System.err.println("Parsing failed.  Reason: " + e.getMessage());
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("Test for Invert", options);
+			System.exit(0);
+		}
+
+		inverter.initializeIndex(documents, dictionary, postingsList);
+		documents = inverter.getDocuments();
+		dictionary = inverter.getDictionary();
+		postingsList = inverter.getPostingsList();
+
+		long duration = System.nanoTime() - startTime;
+		double seconds = (double) duration / 1000000000;
+		System.out.println("Index creation finished. Time: " + seconds + "s");
+
+		//TODO: Change the main program to use Search instead of Test.
+		Search search = new Search(documents, dictionary, postingsList, useStemming, useStopWords, idfThreshold,
+				maxResults);
+		System.out.println("===========================================================================");
+		System.out.println("Enter search term: ");
+
+		try (Scanner scan = new Scanner(System.in)) {
+			while (scan.hasNext()) {
+				String query = scan.nextLine().trim();
+				if (query.equals("zzend")) {
+					System.out.println("Program exiting...");
+					System.out.println(
+							"Average query time for " + timeList.size() + " queries: " + calculateAverage() + "s");
+					System.exit(0);
+				} else {
+					startTime = System.nanoTime();
+					search.displayResults(query);
+					duration = System.nanoTime() - startTime;
+					seconds = (double) duration / 1000000000;
+					System.out.println("Query time: " + seconds + "s");
+					timeList.add(seconds);
+					System.out.println("===========================================================================");
+					System.out.println("Enter search term: ");
+				}
+			}
+		}
+	}
+}
