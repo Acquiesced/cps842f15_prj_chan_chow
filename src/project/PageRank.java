a4d071c9e10511a2e029561fc1fa8059a58dc28a
Acquiesced <itsbradchow@gmail.com>
2015-12-02 16:45:44 -0500

    MatrixMultiply and Surfer methods

--- src/project/PageRank.java
+++ src/project/PageRank.java
@@ -17,22 +17,28 @@ public class PageRank {
 
 	public PageRank(HashMap<Integer, DocumentRecord> documentRecordList) {
 		this.documentRecordList = documentRecordList;
-		this.sortedDocumentRecordList = new TreeMap<Integer, DocumentRecord>(documentRecordList);
+		this.sortedDocumentRecordList = new TreeMap<Integer, DocumentRecord>(
+				documentRecordList);
 	}
 
 	public void citationListInit() {
-		for (Entry<Integer, DocumentRecord> docEntry : documentRecordList.entrySet()) {
+		for (Entry<Integer, DocumentRecord> docEntry : documentRecordList
+				.entrySet()) {
 			// System.out.println(docEntry.getValue().getCitationList());
-			String[] recordCitationArr = docEntry.getValue().getCitationList().split("h\n");
+			String[] recordCitationArr = docEntry.getValue().getCitationList()
+					.split("h\n");
 			for (String citation : recordCitationArr) {
 				String[] citationStringArr = citation.split("\\s+");
-				Integer outLinkDocumentID = Integer.valueOf(citationStringArr[0]);
+				Integer outLinkDocumentID = Integer
+						.valueOf(citationStringArr[0]);
 				Integer documentID = Integer.valueOf(citationStringArr[2]);
 				// System.out.println(outLinkDocumentID + " " + documentID);
-				citationList.add(new CitationRecord(outLinkDocumentID, documentID));
+				citationList.add(new CitationRecord(outLinkDocumentID,
+						documentID));
 			}
 		}
 		double[][] pMatrix = probabilityMatrixInit(adjacencyMatrixInit(citationList));
+		randomSurfer(pMatrix);
 	}
 
 	private double[][] adjacencyMatrixInit(Set<CitationRecord> citationList) {
@@ -43,9 +49,8 @@ public class PageRank {
 		}
 
 		for (CitationRecord citRecord : citationList) {
-			aMatrix[citRecord.getDocumentID() - 1][citRecord.getOutLinkDocumentID() - 1] = 1;
-			System.out.println(citRecord.getDocumentID() + " " + citRecord.getOutLinkDocumentID() + " "
-					+ aMatrix[citRecord.getDocumentID() - 1][citRecord.getOutLinkDocumentID() - 1]);
+			aMatrix[citRecord.getDocumentID() - 1][citRecord
+					.getOutLinkDocumentID() - 1] = 1;
 		}
 		return aMatrix;
 	}
@@ -77,29 +82,45 @@ public class PageRank {
 		return aMatrix;
 	}
 
-	private void randomSurfer(double[][] pMatrix) {
+	private double[][] randomSurfer(double[][] pMatrix) {
+		int matrixSize = sortedDocumentRecordList.lastKey();
+		double[][] pVector = new double[1][matrixSize];
+		Arrays.fill(pVector[0], 0);
+		pVector[0][0] = 1.0;
+		for (int i = 0; i < 10; i++) {
+			pVector = multiplyMatrix(pVector, pMatrix);
+		}
+		double[][] pageRank = pVector;
+		for(int i =0; i<pageRank[0].length; i++){
+			System.out.printf("%.10f ",pageRank[0][i]);
 
+		}
+		return pageRank;
 	}
 
 	private double[][] multiplyMatrix(double[][] pVector, double[][] pMatrix) {
 		int pVectorRows = pVector.length;
-	    int pVectorcols = pVector[0].length;
-	    int pMatrixrows = pMatrix.length;
-	    int pMatrixcols = pMatrix[0].length;
-	         double[][] result = new double[pVectorRows][pMatrixcols];
-	         for (int i=0; i< pVectorRows; i++){
-	            for (int j=0; j< pMatrixcols; j++){
-	               for (int k=0; k< pVectorcols; k++){
-	                  result[i][j] += pVector[i][k] * pMatrix[k][j];
-	                  return result;
-	               }
-	            }
-	         }
+		int pVectorcols = pVector[0].length;
+		int pMatrixrows = pMatrix.length;
+		int pMatrixcols = pMatrix[0].length;
+		if (pVectorcols != pMatrixrows)
+			throw new RuntimeException("Illegal matrix dimensions.");
+		double[][] result = new double[pVectorRows][pMatrixcols];
+		for (int i = 0; i < pVectorRows; i++) {
+			for (int j = 0; j < pMatrixcols; j++) {
+				for (int k = 0; k < pVectorcols; k++) {
+					result[i][j] += pVector[i][k] * pMatrix[k][j];
+				}
+			}
+		}
+		return result;
+
 	}
 
 	public void printCitation() {
 		for (CitationRecord citRecord : citationList) {
-			System.out.println(citRecord.getOutLinkDocumentID() + " " + citRecord.getDocumentID());
+			System.out.println(citRecord.getOutLinkDocumentID() + " "
+					+ citRecord.getDocumentID());
 		}
 	}
 }
