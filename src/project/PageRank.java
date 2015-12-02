adc4bee82fd6e9c997874ecaf3fa034171ed500e
epq <aerial.exe@gmail.com>
2015-12-02 14:41:57 -0500

    Fixed bounds of probability matrix. Changed citationlist arraylist to set.

--- src/project/PageRank.java
+++ src/project/PageRank.java
@@ -3,21 +3,27 @@ package src.project;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map.Entry;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class PageRank {
 	final static double DAMPINGFACTOR = 0.85;
 	HashMap<Integer, DocumentRecord> documentRecordList;
-	ArrayList<CitationRecord> citationList = new ArrayList<CitationRecord>();
-
+	TreeMap<Integer, DocumentRecord> sortedDocumentRecordList;
+	//ArrayList<CitationRecord> citationList = new ArrayList<CitationRecord>();
+	Set<CitationRecord> citationList = new HashSet<CitationRecord>();
+	
 	public PageRank(HashMap<Integer, DocumentRecord> documentRecordList) {
 		this.documentRecordList = documentRecordList;
+		this.sortedDocumentRecordList = new TreeMap<Integer, DocumentRecord>(documentRecordList);
 	}
 
 	public void citationListInit() {
 		for (Entry<Integer, DocumentRecord> docEntry : documentRecordList.entrySet()) {
 			// System.out.println(docEntry.getValue().getCitationList());
-			String[] recordCitationArr = docEntry.getValue().getCitationList().split("\n");
+			String[] recordCitationArr = docEntry.getValue().getCitationList().split("h\n");
 			for (String citation : recordCitationArr) {
 				String[] citationStringArr = citation.split("\\s+");
 				Integer outLinkDocumentID = Integer.valueOf(citationStringArr[0]);
@@ -29,8 +35,8 @@ public class PageRank {
 		adjacencyMatrixInit(citationList);
 	}
 
-	private void adjacencyMatrixInit(ArrayList<CitationRecord> citationList) {
-		int matrixBounds = documentRecordList.size();
+	private void adjacencyMatrixInit(Set<CitationRecord> citationList) {
+		int matrixBounds = sortedDocumentRecordList.lastKey();
 		int[][] aMatrix = new int[matrixBounds][matrixBounds];
 		for (int[] row : aMatrix) {
 			Arrays.fill(row, 0);
@@ -38,7 +44,7 @@ public class PageRank {
 
 		for (CitationRecord citRecord : citationList) {
 			aMatrix[citRecord.getDocumentID() - 1][citRecord.getOutLinkDocumentID() - 1] = 1;
-			//System.out.println(citRecord.getDocumentID() + " " + citRecord.getOutLinkDocumentID() + " " +  aMatrix[citRecord.getDocumentID() - 1][citRecord.getOutLinkDocumentID() - 1] );
+			System.out.println(citRecord.getDocumentID() + " " + citRecord.getOutLinkDocumentID() + " " +  aMatrix[citRecord.getDocumentID() - 1][citRecord.getOutLinkDocumentID() - 1] );
 		}
 	}
 
